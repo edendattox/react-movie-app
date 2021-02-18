cd3ff4efeb1dafc4ac188b95ed00445568bc7b30
rahul kathait <edendattox@gmail.com>
2021-02-19 01:13:32 +0530

    app completed

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -2,14 +2,25 @@ import React from "react";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
+const setVoteClass = vote => {
+  if (vote >=8 ) {
+    return "green";
+  } else if (vote >= 6) {
+    return "orange";
+  } else {
+    return "red";
+  }
+} 
+
 const Movie = ({ title, overview, poster_path, vote_average }) => {
+
   return (
     
     <div className="movie">
-      <img src={IMG_API + poster_path} alt={title}/>
+      <img src={poster_path ? (IMG_API + poster_path) : "https://images.unsplash.com/photo-1585951237313-1979e4df7385?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"}alt={title}/>
       <div className="movie-info">
         <h3>{title}</h3>
-        <span>{vote_average}</span>
+        <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
       </div>
 
       <div className="movie-over">
