defe08f3b061d8d346c2a87996e3c02e4381dfad
rahul kathait <edendattox@gmail.com>
2021-02-19 00:53:53 +0530

    added api and added search bar

--- src/App.js
+++ src/App.js
@@ -5,18 +5,64 @@ import Movie from "../src/component/Movie";
 
 const FEATURED_API =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2b83cbbfed0be4545a7d77bc8d2f75da&page=1";
-const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2b83cbbfed0be4545a7d77bc8d2f75da&query=";
+  "https://api.themoviedb.org/3/search/movie?&api_key=2b83cbbfed0be4545a7d77bc8d2f75da&query=";
+  
 
 const App = () => {
-  const movies = [1, 2, 3];
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+   getMovies(FEATURED_API)
+  }, []);
+
+  const getMovies =(API) => {
+    fetch(API)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setMovies(data.results);
+    });
+  }
+
+  const handelOnSubmit = (e) => {
+    e.preventDefault();
+  
+    if(searchTerm) {
+      fetch(SEARCH_API + searchTerm)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setMovies(data.results);
+      });
+      setSearchTerm("");
+    }
+  }
+
+  const handelOnChange = e => {
+    setSearchTerm(e.target.value);
+  }
+
   return (
-    <div className="App">
-      {movies.map((movie) => (
-        <Movie />
-      ))}
+    <>
+    <header>
+
+    <form onSubmit={handelOnSubmit}>
+    <input 
+     type="search" 
+     onChange={handelOnChange}
+     className="search" 
+     placeholder="search..." 
+     value={searchTerm} />
+  </form>
+
+  </header>
+    <div className="movie-container">
+      {movies.length > 0 &&
+        movies.map((movie) => <Movie key={movie.id} {...movie} />)}
     </div>
+  </>
   );
 };
 
